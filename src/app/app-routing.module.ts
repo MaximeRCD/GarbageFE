378be079e4f9f187c22da9b364aca6ae721d0bed
eugenepascal <pasou1000@gmail.com>
2023-01-28 20:16:34 +0100

    other files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LogInComponent} from "./connection/log-in/log-in.component";
+import {RegisterComponent} from "./connection/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LogInComponent },
+  { path: 'register', component: RegisterComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { WebcamModule } from 'ngx-webcam';
 
@@ -7,16 +7,27 @@ import { AppComponent } from './app.component';
 import { ScanPhotoComponent } from './scan-photo/scan-photo.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
+import { RegisterComponent } from './connection/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import {LogInComponent} from "./connection/log-in/log-in.component";
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ScanPhotoComponent,
-    ProfileComponent
+    ProfileComponent,
+    LogInComponent,
+    RegisterComponent
   ],
   imports: [
+    FlexLayoutModule,
     BrowserModule,
     RouterModule.forRoot([
       {path: 'Profile', component: ProfileComponent},
@@ -25,9 +36,13 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     WebcamModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }

1c1b37d62ca8a17b64edf3f7eb91ed5555703a70
Elyes <elyess.laiche@gmail.com>
2023-01-28 16:40:26 +0100

    DataViz dechets fonctionnel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { WebcamModule } from 'ngx-webcam';
 
@@ -9,18 +9,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ScanPhotoComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'Profile', component: ProfileComponent},
-      {path: '', component: ScanPhotoComponent},
+      {path: 'Scan Waste', component: ScanPhotoComponent},
+      {path: '', component: HomeComponent},
     ]),
     AppRoutingModule,
     WebcamModule,
@@ -28,6 +31,7 @@ import { RouterModule } from '@angular/router';
     ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }

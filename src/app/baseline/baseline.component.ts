fb1d28f0921315e1f5a6f978dee726aa9f618950
maxime richaudeau <maxime.richaudeau1@gmail.com>
2023-01-31 22:38:00 +0100

    Create a component to handle nav and footer

---
+++ src/app/baseline/baseline.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baseline',
+  templateUrl: './baseline.component.html',
+  styleUrls: ['./baseline.component.css']
+})
+export class BaselineComponent {
+
+  isConnected = true;
+  user: any;
+
+  LogOut() {
+    
+  }
+}

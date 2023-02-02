b0d36c43615af3e6709b0e81e0b8abb4e3a66b19
Elyes <elyess.laiche@gmail.com>
2023-02-02 11:00:56 +0100

    New tableau

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent {
   // Inherit attributes from the parent component
   @Input() dashboardIndex = 0;
   @Input() toolbar = 'hidden';
-  @Input() vizUrl = 'https://public.tableau.com/views/GarbageFE/Tableaudebord1?:language=fr-FR&publish=yes&:display_count=n&:origin=viz_share_link';
+  @Input() vizUrl = 'https://public.tableau.com/views/GarbageFE/Evolutiondesdechets?:language=fr-FR&publish=yes&:display_count=n&:origin=viz_share_link';
 
   // Dashboard properties
   public VizIndex = 'Tableau-Viz-' + this.dashboardIndex;
